2e1afe7da14b99b7b5be9e62131fb1f39fca9ce9
eduardosantospro <eduardo@dossantos.pro>
2020-12-16 02:58:30 +0000

    Criar a base de dados e tabela produtos

---
+++ LojaCet49/Dados/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using LojaCet49.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaCet49.Dados
+{
+    public class DataContext : DbContext                        /* o meu DataContext vai buscar o conteúdo ao DbContext da Entity Framework Core  */
+    {
+        /* Criar tabela: a propriedade DbSet vai criar uma tabela chamada Produtos usando o modelo Produto (Produto.cs)
+         * O Produto é o Produto.cs (estrutura)
+         * Produtos é a tabela (registos)
+         * { get; set; } por permite colocar e buscar produtos
+        */
+        public DbSet<Produto> Produtos { get; set; }
+
+        /*
+         * options é o nome que quiseremos da variavel
+         * 
+         * O meu DataContext vai usar as opções do DataContext dele que estão na base (DbContext) 
+         * Este processo chama-se HERANÇA porque o objecto herda todas as funcionalidades do DbContext da Entity Framework Core
+         */
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ LojaCet49/Dados/Entidades/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaCet49.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }                                                         /* propriedade de leitura e escrita get: pode ler, set: pode escrever*/
+
+
+        public string Nome { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]         /* Formatação moeda (currency) que não mostra na edição*/
+        public decimal Preco { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string UrlDaImagem { get; set; }
+
+
+        [Display(Name = "Última compra")]
+        public DateTime UltimaCompra { get; set; }
+
+
+        [Display(Name = "Última venda")]
+        public DateTime UltimaVenda { get; set; }
+
+
+        [Display(Name = "Disponibilidade")]
+        public bool Disponivel { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
